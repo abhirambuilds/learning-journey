a927704d48cf5b24da4b5421387dc9c703a3f520
KOLAKALAPUDI ABHIRAM REDDY <abhiram.builds@gmail.com>
2026-07-28 02:24:35 +0530

    Time: 2 ms (92.11%), Space: 47.7 MB (76.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int j=-1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
+            if(nums[i]==0) {
                 j=i;
                 break;
             }
