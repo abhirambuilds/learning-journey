8cead6622e8d2b725caa847a0fd88959416971d1
KOLAKALAPUDI ABHIRAM REDDY <abhiram.builds@gmail.com>
2026-07-28 02:39:16 +0530

    Time: 0 ms (100%), Space: 47.5 MB (32.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum = 0, num, n = nums.length;
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i];
+        int xor=nums.length;
+        for(int i=0;i<nums.length;i++){
+            xor^=i;
+            xor^=nums[i];
         }
-        num = (n*(n+1)/2) - sum;
-        return num;
+        return xor;
     }
 }
