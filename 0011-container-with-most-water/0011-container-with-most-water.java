93ebcc5328eb8b4a050e2cb9d6d3c9d8ca9c3643
KOLAKALAPUDI ABHIRAM REDDY <abhiram.builds@gmail.com>
2026-08-06 01:45:55 +0530

    Time: 5 ms (82.96%), Space: 77.6 MB (12.84%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+       int maxArea=0;
+       int left=0;
+       int right=height.length-1;
+       while(left<right){
+            int width=right-left;
+            int currHeight=Math.min(height[left],height[right]);
+            int currArea=width*currHeight;
+            maxArea=Math.max(currArea,maxArea);
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+       } 
+       return maxArea;
+    }
+}
