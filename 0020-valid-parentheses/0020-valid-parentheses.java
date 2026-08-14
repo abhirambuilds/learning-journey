233f2fbe9826a5e89362fdc6ed8439d5751a3c65
KOLAKALAPUDI ABHIRAM REDDY <abhiram.builds@gmail.com>
2026-08-15 01:39:31 +0530

    Time: 3 ms (86.11%), Space: 42.9 MB (93.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
         for(char c:s.toCharArray()){
             if(c=='(') stack.push(')');
             else if(c=='{') stack.push('}');
