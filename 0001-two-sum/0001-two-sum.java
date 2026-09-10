60b71c1b4c5fc443f5919498cc950f6510c63523
KOLAKALAPUDI ABHIRAM REDDY <abhiram.builds@gmail.com>
2026-09-11 00:30:11 +0530

    Time: 3 ms (59.19%), Space: 47.5 MB (11.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,11 +3,10 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int comp=target-nums[i];
-            if(map.containsKey(comp)) 
-            {   return new int[] {i,map.get(comp)};
-            }
-            else map.put(nums[i],i); 
+            if(map.containsKey(comp)){
+                return new int[] {i,map.get(comp)};
+            }   else map.put(nums[i],i);
         }
-        return new int[] {};
+        return new int [] {};
     }
 }
